467f1442a68e969b4ac24f5bd8a7d85c20aee791
Leodesignss <104667935+Leodesignss@users.noreply.github.com>
2024-04-11 03:02:56 +0700

    BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,11 +1,11 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'Leonardo Dicaprio',
+        name: 'leonardo dicaprio',
         username: '@dcapss',
         url: 'https://link.dcaps.space',
         titleImg: true,
-        avatar: '/avatar.png',
+        avatar: 'https://scontent.cdninstagram.com/v/t51.2885-19/425032040_2165361613817334_2628927631881880982_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=106&_nc_ohc=Nx_2WsofPNUAb7KolpT&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfAAHBT-w9A_CT_nP_HV5N8H4MgCNz5xrVjbpzRjbmkFzQ&oe=661CC60D&_nc_sid=10d13b',
         nftAvatar: true,
         description: 'check out my social media n portofolio ',
         descShow: true,
