8948883ea4d0d335b8adcb012d254ce483990ef1
Leodesignss <104667935+Leodesignss@users.noreply.github.com>
2024-04-11 03:20:30 +0700

    next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,23 +2,23 @@ export default {
     openGraph: {
         type: 'website',
         locale: 'en_IE',
-        title: 'next lnks by realvjy',
-        url: 'https://vjy.me',
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk.',
+        title: '@dcapss links',
+        url: 'https://link.dcaps.space',
+        description: '@dcapss link n portofolio website',
         locale: 'en_EN',
         keywords: 'designer, product designer, illustrator, indian designer, vijay verma, realvjy, illlustrations, indian illustrator, ui designer india, sushi design system, zomato designer, 3dicons, uiprint, vijay realvjy, indian ui designer, design india, overlayz, nft art',
         images: [
             {
                 width: 1200,
                 height: 630,
-                url: `https://vjy.me/preview.jpg`,
+                url: `https://cdn.inflact.com/media/425032040_2165361613817334_2628927631881880982_n.jpg?url=https%3A%2F%2Fscontent.cdninstagram.com%2Fv%2Ft51.2885-19%2F425032040_2165361613817334_2628927631881880982_n.jpg%3Fstp%3Ddst-jpg_s320x320%26_nc_ht%3Dscontent-hbe1-1.cdninstagram.com%26_nc_cat%3D106%26_nc_ohc%3DNx_2WsofPNUAb41UniI%26edm%3DAOQ1c0wBAAAA%26ccb%3D7-5%26oh%3D00_AfBg159Y4rPxCmEV-zQ2JY1QmpXEAQx-bLC0sZUVPidVhw%26oe%3D661CC60D%26_nc_sid%3D8b3546&time=1712775600&key=f615dc5e7c702713281c1eb41c0bc70e`,
             },
         ],
-        site_name: 'realvjy',
+        site_name: '@dcapss',
     },
     twitter: {
-        handle: '@realvjy',
-        site: 'vjy.me',
+        handle: '',
+        site: 'link.dcaps.space',
         cardType: 'summary_large_image',
     },
 };
